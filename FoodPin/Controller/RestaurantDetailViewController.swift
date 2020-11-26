4e22f0f9b8cef2baba25bdb03fc55c8e69e99c39
DavidTan14 <410621214@gms.ndhu.edu.tw>
2020-11-26 11:27:46 +0800

    Initial Commit

---
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2020/11/5.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+    
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    
+    var restaurant = Restaurant()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        
+        //Configure header view
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
+              
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
